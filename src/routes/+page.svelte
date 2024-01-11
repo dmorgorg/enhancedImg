1c41aeed1b13281cf5838bc5f108acb8fc761dcc
dave morgan <dave@dmorg.org>
2024-01-11 08:16:36 +0000

    enhancer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,3 @@
-<script>
-  const pictures = import.meta.glob(
-    "./images/*.{avif,gif,heif,jpeg,jpg,png,tiff,webp}",
-    {
-      query: { enhanced: true },
-    }
-  )
-
-  console.log(pictures)
-</script>
-
 <enhanced:img src="./images/2022-08-04-042944.jpg" alt="1" />
 
 <enhanced:img src="./images/2022-08-04-044035.jpg" alt="2" />
