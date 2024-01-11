cdf12fb6cb859f3fa3ea80bfb4c86bf97c1e9e39
dave morgan <dave@dmorg.org>
2024-01-11 08:21:34 +0000

    try again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,3 @@
 <enhanced:img src="./images/2022-08-04-042944.jpg" alt="1" />
 
 <enhanced:img src="./images/2022-08-04-044035.jpg" alt="2" />
-
-<enhanced:img
-  src="./images/2022-08-04-041300.jpg?w=1280;640;400"
-  sizes="(min-width:1920px) 1280px, (min-width:1080px) 640px, (min-width:768px) 400px"
-/>
-
-<style>
-  img {
-    width: 100%;
-    height: auto;
-    /* width: auto;
-		height: 100%; */
-  }
-</style>
