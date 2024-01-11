4db2057d90ba0f48447f1350a83fa9d88a5de56f
dave morgan <dave@dmorg.org>
2024-01-11 08:25:45 +0000

    style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
 <enhanced:img src="./images/2022-08-04-042944.jpg" alt="1" />
 
 <enhanced:img src="./images/2022-08-04-044035.jpg" alt="2" />
+
+<style>
+  :global(img) {
+    width: 100%;
+    height: auto;
+  }
+</style>
