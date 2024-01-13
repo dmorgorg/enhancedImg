aa8c7cda9c956fa9677d0aa7fb6fe5fa5fb74651
dave morgan <dave@dmorg.org>
2024-01-13 06:56:07 +0000

    testing

--- src/routes/blog/one/+page.svelte
+++ src/routes/blog/one/+page.svelte
@@ -1,11 +1,13 @@
 <enhanced:img
   src="$lib/images/2022-08-06-041300.jpg?w=1280;640;400"
   sizes="(min-width:1920px) 1280px, (min-width:1080px) 640px, (min-width:768px) 400px"
+  alt="A picture of a cat"
 />
 
 <enhanced:img
   src="$lib/images/2022-08-06-041508.jpg"
   sizes="min(1280px, 100vw)"
+  alt="A picture of a mouse"
 />
 
 <style>
