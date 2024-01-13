b11ffdba2d13cc7ae57b22c492925b38ff98115f
dave morgan <dave@dmorg.org>
2024-01-13 06:48:42 +0000

    saturday test

---
+++ src/routes/blog/one/+page.svelte
@@ -0,0 +1,16 @@
+<enhanced:img
+  src="$lib/images/2022-08-06-041300.jpg?w=1280;640;400"
+  sizes="(min-width:1920px) 1280px, (min-width:1080px) 640px, (min-width:768px) 400px"
+/>
+
+<enhanced:img
+  src="$lib/images/2022-08-06-041508.jpg"
+  sizes="min(1280px, 100vw)"
+/>
+
+<style>
+  :global(img) {
+    width: 100%;
+    height: auto;
+  }
+</style>
